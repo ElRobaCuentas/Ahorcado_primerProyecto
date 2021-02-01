b2fb027d83e86c4e68a40c5d6a6c7f5be85c12e4
Juan Jesus ML <juanjesusmatias06@gmail.com>
2021-02-01 16:40:34 -0500

    se desarrollo los estilos del proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ 
+
+  constructor() {
+    console.log('Se acaba de crear el App Component'); 
+  }
+}
